15abd356a0dc2c90cf71287c4397ed5e67d48889
Erin O'Neill <ekelenc23@gmail.com>
2017-08-22 16:08:40 -0600

    Add files via upload

---
+++ search.py
@@ -0,0 +1,36 @@
+'''recursively search a directory for a file, and update a line of text'''
+
+import os
+
+user_dir = input(' ')  #prompt for user 
+root_dir = os.path.join(user_dir)
+
+match_file = input('') #prompt for user to pass in 
+replace_file = '' #assign a generic file name here
+
+wordReplacements1 = input('Find word: ')
+wordReplacements2 = input('Replace with word: ')
+
+wordReplacements = {wordReplacements1 : wordReplacements2} #dictionary creation of text to find and replace
+
+def updateSCM(line):
+    for key, value in wordReplacements.items(): #instructing the wordReplacement/what to find
+        line = line.replace(key, value)
+    return line
+
+def setPath(dirpath, filename):
+    org_name = os.path.join(dirpath, filename) 
+    new_name = os.path.join(dirpath, filename.replace(match_file, replace_file)) #directions for the new name, replace pom.xml with pom2.xml
+    with open(new_name, 'w') as output_file, open(org_name) as input_file: 
+        for line in input_file:
+            output_file.write(updateSCM(line))
+	
+	input_file.close()
+	os.remove(match_file)
+	os.rename(new_name, org_name)
+  
+			
+for dirpath, dirs, files in os.walk(root_dir):
+    for filename in files:
+        if (filename == match_file):
+            setPath(dirpath, filename)
